e4dd3015b3b6b3f84d38f95e1afe4d283ac2bb70
Radwa-Saeed <61630444+Radwa-Saeed@users.noreply.github.com>
2022-08-17 15:11:52 +0200

    server app

---
+++ Django_Server/blog_api/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from rest_framework import generics
+from blog.models import Post
+from blog_api.serializers import PostSerializer
+# from .serializers import PostSerializer
+# Create your views here.
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.postobjects.all()
+    serializer_class = PostSerializer
+
+class PostDetail(generics.RetrieveDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
